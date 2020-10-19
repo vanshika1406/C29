385a5b011e47d559fe2dfa11cbb847ef89b10dd6
Vanshika1406 <vanshika.turkar@outlook.com>
2020-10-18 17:47:30 -0700

    We added a slingshotgit add Slingshot.js sketch.js

--- Slingshot.js
+++ Slingshot.js
@@ -6,7 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+        this.sling1= loadImage("sprites/sling1.png");
+        this.sling2= loadImage("sprites/sling2.png");
+        this.sling3= loadImage("sprites/sling3.png");
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,11 +19,31 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 200, 20);
+        image(this.sling2, 170, 20);
+       
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            //strokeWeight(4);
+        
+            stroke(48, 23, 8);
+            if(pointA.x<220){
+                strokeWeight(7);
+                line(pointA.x-25, pointA.y, pointB.x-10, pointB.y-35);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3-35)
+                image(this.sling3, pointA.x-30, pointA.y-10, 15, 30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x-25, pointA.y, pointB.x-10, pointB.y-35);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3-35)
+                image(this.sling3, pointA.x-30, pointA.y-10, 15, 30);
+            }
+            
+            pop();
+
         }
     }
     
